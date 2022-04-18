8a3626de0d960b500a4c14ae79a5efc0379e314e
Eliot <eliotpitman@gmail.com>
2022-04-18 13:04:56 -0500

    adds filter method and autocomplete

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -1,11 +1,13 @@
 <script>
 import axios from "axios";
+import moment from "moment";
 
 export default {
   data: function () {
     return {
       posts: [],
       currentPosts: {},
+      titleFilter: "",
     };
   },
   created: function () {
@@ -14,20 +16,39 @@ export default {
       console.log("All posts", this.posts);
     });
   },
+  methods: {
+    dateMethod: function (date) {
+      return moment(date).fromNow();
+    },
+    filterPosts: function () {
+      return this.posts.filter((post) => {
+        var lowerTitle = post.title.toLowerCase();
+        var lowerTitleFilter = this.titleFilter.toLowerCase();
+        return lowerTitle.includes(lowerTitleFilter);
+      });
+    },
+  },
 };
 </script>
 
 <template>
-  <h1>here are all posts</h1>
-  <div v-for="post in posts" :key="post.id">
-    <div class="card col-sm-4 mb-4" style="width: 18rem">
-      <img :src="`${post.image}`" class="card-img-top" />
-      <div class="card-body">
-        <h5 class="card-title">post: {{ post.title }}</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-        <a :href="`/posts/${post.id}`">click for more info</a>
+  <h1>here are all the posts</h1>
+  <div class="row">
+    <input v-model="titleFilter" type="text" list="titles" />
+    <datalist id="titles">
+      <option v-for="post in posts" v-bind:key="post.id">{{ post.title }}</option>
+    </datalist>
+    <div v-for="post in filterPosts()" @click="currentPosts = post" v-bind:key="post.id" class="card col-sm-4 mb-4">
+      <div class="card col-sm-4 mb-4" style="width: 18rem">
+        <img :src="`${post.image}`" class="card-img-top" />
+        <div class="card-body">
+          <h5 class="card-title">post: {{ post.title }}</h5>
+          <h5 class="card-title">created: {{ dateMethod(post.created_at) }}</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a :href="`/posts/${post.id}`">click for more info</a>
+        </div>
       </div>
     </div>
   </div>
